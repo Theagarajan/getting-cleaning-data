2922cb898dacd1851f9b460e41b9c19d910a9377
Roman Nefyodov <rnefyodov@yandex-team.ru>
2014-04-28 03:15:13 +0400

    init

---
+++ run_analysis.R
@@ -0,0 +1,22 @@
+features <- read.table('features.txt', col.names=c('id', 'name'))
+interesting_features <- grep('mean\\(\\)|std\\(\\)', features$name)
+
+read_data <- function(dir) {
+  act_labels <- read.table('activity_labels.txt', col.names=c('id', 'activity'))
+  
+  data <- read.table(sprintf('%s/X_%s.txt', dir, dir), col.names=features$name)
+  data <- data[,interesting_features]
+  
+  data$activity <- merge(act_labels, read.table(sprintf('%s/y_%s.txt', dir, dir), col.names='id'))$activity
+  data$subject <- read.table(sprintf('%s/subject_%s.txt', dir, dir))[,1]
+  data
+}
+
+library(plyr)
+
+test_data <- read_data('test')
+train_data <- read_data('train')
+
+full_data <- rbind.fill(test_data, train_data)
+
+agg_data <- aggregate(full_data, by=full_data[c("activity", "subject")], FUN=mean)
